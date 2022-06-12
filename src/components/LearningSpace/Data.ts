f66f4d2c0f77b90370198f01dccde3ad43b524ae
ljenchik <pelagenkolena@gmail.com>
2022-06-12 17:19:58 +0100

    Added buttons and question

--- src/components/LearningSpace/Data.ts
+++ src/components/LearningSpace/Data.ts
@@ -1,26 +1,38 @@
 export const Data = [
-    {
-        question: "What?", answers : 
+    {  
+        title: `Moons of Mars`,
+        information : `Mars has two small moons: Phobos and Deimos. 
+                Phobos (fear) and Deimos (panic) were named after the horses 
+                that pulled the chariot of the Greek war god Ares, 
+                the counterpart to the Roman war god Mars. 
+                Both Phobos and Deimos were discovered in 1877 
+                by American astronomer Asaph Hall. The moons appear to have 
+                surface materials similar to many asteroids in the outer asteroid belt, 
+                which leads most scientists to believe 
+                that Phobos and Deimos are captured asteroids`,
+        question: "How many moons does Mars have?", 
+        answers : 
             [
-                {text: "asd", correct:true}, 
-                {text:"sdf", correct:false},
-                {text:"dfg", correct:false}
+                {text: "2", correct:true}, 
+                {text:"3", correct:false},
+                {text:"4", correct:false},
+                {text:"5", correct:false}
             ]
-        }, 
-        {
-            question: "Why?", answers : 
+    }, 
+    {
+        question: "Why?", answers : 
             [
                 {text:"zxc", correct: false},
                 {text:"xcv", correct:false},
                 {text:"cvb", correct:true}
             ]
-        },
-        {
-            question: "Who?", answers : 
+    },
+    {
+        question: "Who?", answers : 
             [
                 {text:"jki", correct:false},
                 {text:"fghfj,", correct:true},
                 {text:"zxc", correct: false}
             ]
-        }
-    ] 
+    }
+] 
--- src/components/LearningSpace/LearningSpace.tsx
+++ src/components/LearningSpace/LearningSpace.tsx
@@ -10,8 +10,9 @@ export function LearningSpace() {
   const question = Data[questionIndex];
   return (
     <div>
-      <div>Information about space and image</div>
-      <div>{question.question}</div>
+      <div className="question">{question.title}</div>
+      <div className="information">{question.information}</div>
+      <div className="question">{question.question}</div>
       {question.answers.map((answer) => (
         <div
           onClick={() => {
@@ -20,14 +21,35 @@ export function LearningSpace() {
           className={
             selectedAnswer === answer
               ? answer.correct
-                ? "highlight-correct"
-                : "highlight-incorrect"
-              : ""
+                ? "highlight-correct answer"
+                : "highlight-incorrect answer"
+              : "answer"
           }
         >
           {answer.text}
         </div>
       ))}
+      <div></div>
+      <div>
+        <button className="previous-button"
+          onClick={() => {
+            questionIndex > 0
+              ? setQuestionIndex(questionIndex - 1)
+              : setQuestionIndex(questionIndex);
+          }}
+        >
+          Previous
+        </button>
+        <button className="next-button"
+          onClick={() => {
+            questionIndex < Data.length - 1
+              ? setQuestionIndex(questionIndex + 1)
+              : setQuestionIndex(questionIndex);
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
