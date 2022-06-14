72045835b1b7014e85b34acba232e7b4bcc7f243
ljenchik <pelagenkolena@gmail.com>
2022-06-14 09:58:40 +0100

    Added MarsHoliday Image and styling

--- src/components/MarsHolidays/MarsHolidays.tsx
+++ src/components/MarsHolidays/MarsHolidays.tsx
@@ -1,7 +1,9 @@
+import "./MarsHolidays.scss"
 export function MarsHolidays() {
   return (
     <div>
-      <h1>Would you dare to go to Mars?</h1>
+      <h2>Would you dare go to Mars?</h2>
+      <img src="https://airnfts.s3.amazonaws.com/nft-images/202110/Ticket_to_the_Mars_1620604616509.jpg" className="ticket"/>
     </div>
   );
 }
