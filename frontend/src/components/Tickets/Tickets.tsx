12b4d764e61afbbbcc20e28340592985acf3a3b9
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2022-11-21 17:20:14 +0000

    Added minor styling

--- frontend/src/components/Tickets/Tickets.tsx
+++ frontend/src/components/Tickets/Tickets.tsx
@@ -4,6 +4,8 @@ import { useNavigate, useParams } from "react-router-dom";
 import { Ticket } from "../../models";
 import "./Tickets.scss"
 import {TicketDisplay } from "../Ticket/TicketDisplay";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRocket, faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
 
 export const Tickets = () => {
   const navigate = useNavigate();
@@ -31,6 +33,17 @@ export const Tickets = () => {
             return (
                 <div className="tickets-list">
                   <TicketDisplay ticket={ticket}/>
+                  <div className="tickets-display-icons">
+                      <FontAwesomeIcon icon={faUserAstronaut} size={"2x"} color={"white"}/>
+                      <FontAwesomeIcon icon={faRocket} size={"2x"}  color={"white"}/>
+                      <FontAwesomeIcon icon={faUserAstronaut} size={"2x"}  color={"white"}/>
+                      <FontAwesomeIcon icon={faRocket} size={"2x"}  color={"white"}/>
+                      <FontAwesomeIcon icon={faUserAstronaut} size={"2x"}  color={"white"}/>
+                      <FontAwesomeIcon icon={faRocket} size={"2x"}  color={"white"}/>
+                      <FontAwesomeIcon icon={faUserAstronaut} size={"2x"}  color={"white"}/>
+                      <FontAwesomeIcon icon={faRocket} size={"2x"}  color={"white"}/>
+                      <FontAwesomeIcon icon={faUserAstronaut} size={"2x"}  color={"white"}/>
+                  </div>
                 </div>
             );
           })}
